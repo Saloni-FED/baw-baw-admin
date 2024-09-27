8d3ff33a7a6db6d0590e67a89faafd7873967bcd
Saloni-FED <salonipandey014@gmail.com>
2024-09-27 19:23:46 +0530

    created sidebar and navbar

--- src/app/(routes)/dashboard/page.jsx
+++ src/app/(routes)/dashboard/page.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import DashBoard from '@/components/Dashboard/DashBoard'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div>
+      <DashBoard/>
+    </div>
   )
 }
 
---
+++ src/components/Dashboard/DashBoard.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const DashBoard = () => {
+  return (
+    <div>
+      <div>
+        <ul className="flex md:gap-6 text-baw-baw-g4">
+          <li>Last 24 hours</li>
+          <li>Last Weeks</li>
+          <li>Last months</li>
+          <li>Last Years</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default DashBoard;
